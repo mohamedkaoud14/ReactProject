822d5648af80dcda8a57861c4c96fa69a20b4781
Maymaher <mai.maher003@gmail.com>
2021-03-20 05:10:41 -0400

    contactinfo

---
+++ protofolio/src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import {Component} from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        
+      </header>
+    </div>
+  );
+}
+
+function contactInfo(){
+  return(
+    <div className="contactInfo">
+  <h3>Contact Info</h3>
+  <div >
+    <a>Email</a>
+    <a>GitHub</a>
+  </div>
+
+    </div>
+
+    
+    
+
+
+  );
+
+}
+
+export default App;
